6e6eea2995f45094533bf4dc7376dc84b21f682d
jared.wal.20 <jared.wal.20@cnu.edu>
2023-09-09 15:25:45 -0400

    Initial commit

---
+++ app/src/main/java/com/example/proj1_jared_wal/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.proj1_jared_wal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
